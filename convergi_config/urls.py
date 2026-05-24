fbb17486e8f1a76c701f4dae756165cd39ce82a6
Geovanna-BM <geovannabmeneses@gmail.com>
2026-05-24 17:58:02 -0300

    feat: add URL paths for home, dashboard, companies list, and search views

--- convergi_config/urls.py
+++ convergi_config/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import listar_empresas, SearchView, dashboard, home
 
+admin.site.site_header = "CONVERGI ADMIN"
+admin.site.site_title = "Convergi ADM"  
+admin.site.index_title = "Bem-vindo ao Painel de Controle do Convergi"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),  
+    path('', home, name='home'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('empresas/', listar_empresas, name='empresas'),
+    path('results/', SearchView.as_view(), name="search"),
 ]
+
+
