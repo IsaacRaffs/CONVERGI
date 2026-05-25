46d53342a0e1d1ae406b7c68289e531d4e4b604f
GuinhoOGuigo <francisguialves@gmail.com>
2026-05-25 20:25:02 -0300

    feat: admin UI

--- convergi_config/settings.py
+++ convergi_config/settings.py
@@ -19,6 +19,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Interface do Admin
+    'admin_interface',
+    'colorfield',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
